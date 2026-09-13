8d26f83c1c1d4edd379c95900a77153947e141c0
C123456arlos <118953415+C123456arlos@users.noreply.github.com>
2026-09-13 14:41:37 -0400

    first commit

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -16,7 +16,7 @@ const syncUserCreation = inngest.createFunction(
             email: email_addresses[0].email_address,
             full_name: first_name + ' ' + last_name,
             profile_picture: image_url,
-            user
+            username:username
         }
         await User.create(userData)
     }
